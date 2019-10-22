6a78aaca13e56f8739326056d5e42a40210eb029
cpboii <chongpeng0218@gmail.com>
2019-10-22 09:21:51 +0800

    roll dice with image

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,10 +11,13 @@ import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        diceImage = findViewById(R.id.dice_image)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
@@ -22,19 +26,29 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        val resultText1: TextView = findViewById(R.id.result_text1)
-        val resultText2: TextView = findViewById(R.id.result_text2)
 
+        val diceImage : ImageView = findViewById(R.id.dice_image)
 
         val randomInt = Random().nextInt(6) + 1
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+
         val randomInt1 = Random().nextInt(6)+1
         val randomInt2 = Random().nextInt(6)+1
-        resultText.text = randomInt.toString()
-        resultText1.text = randomInt1.toString()
-        resultText2.text = randomInt2.toString()
+
 
         Toast.makeText(this, "button clicked",
             Toast.LENGTH_SHORT).show()
     }
+
+
 }
