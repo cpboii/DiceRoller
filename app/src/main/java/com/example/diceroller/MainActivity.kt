afa545ae4a1232da3b02f75a9da08a8526a0918c
cpboii <56568332+cpboii@users.noreply.github.com>
2019-10-31 00:01:26 +0800

    Done count up

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -17,6 +17,9 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
+        val countUpButton: Button = findViewById(R.id.countUp_button)
+        countUpButton.setOnClickListener{countUpNumber()}
+
     }
 
 
@@ -36,5 +39,33 @@ class MainActivity : AppCompatActivity() {
 
         Toast.makeText(this, "button clicked",
             Toast.LENGTH_SHORT).show()
+    }
+
+    private  fun countUpNumber(){
+
+        val resultText1 : TextView = findViewById(R.id.result_text)
+        val resultText2 : TextView = findViewById(R.id.result_text1)
+        val resultText3 : TextView = findViewById(R.id.result_text2)
+
+        val countUpNumber1: TextView = findViewById(R.id.countUp_text1)
+        val countUpNumber2: TextView = findViewById(R.id.countUp_text2)
+        val countUpNumber3: TextView = findViewById(R.id.countUp_text3)
+
+        countUpNumber1.text = (Integer.parseInt(resultText1.text.toString())+1).toString()
+        countUpNumber2.text = (Integer.parseInt(resultText2.text.toString())+1).toString()
+        countUpNumber3.text = (Integer.parseInt(resultText3.text.toString())+1).toString()
+
+        if(Integer.parseInt(countUpNumber1.text.toString())>6){
+            countUpNumber1.text="6"
+        }
+
+        if(Integer.parseInt(countUpNumber2.text.toString())>6){
+            countUpNumber2.text="6"
+        }
+
+        if(Integer.parseInt(countUpNumber3.text.toString())>6){
+            countUpNumber3.text="6"
+        }
+
     }
 }
